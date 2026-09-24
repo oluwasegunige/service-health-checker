8baaa70e74603d12497d660ecb01adb9f092d5fa
Oluwasegun Ige <mailshegze@gmail.com>
2026-09-24 15:04:56 +0100

    feat: implement make http request

--- src/service_health_checker/health_check.py
+++ src/service_health_checker/health_check.py
@@ -8,6 +8,7 @@ def health_check(
     service: Annotated[str, typer.Argument(help="The services to be checked")] = ""
 ):
     dns_check(service=service)
-    print("\n")
+    print()
     check_tcp_connection(service, 443)
+    print()
     

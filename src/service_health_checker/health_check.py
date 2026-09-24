42316297688996240921e592ba12045e9f485966
Oluwasegun Ige <mailshegze@gmail.com>
2026-09-24 17:45:58 +0100

    refactor: seperate logging config

--- src/service_health_checker/health_check.py
+++ src/service_health_checker/health_check.py
@@ -1,6 +1,7 @@
 import typer, logging
 from typing_extensions import Annotated
 
+from .logging_config import logger
 from .dns_check import dns_check
 from .check_tcp_connection import check_tcp_connection
 from .make_http_request import make_http_request
@@ -8,35 +9,30 @@ from .make_http_request import make_http_request
 def health_check(
     service: Annotated[str, typer.Argument(help="The services to be checked")] = ""
 ):
-    logging.basicConfig(
-        level=logging.INFO, 
-        format='%(asctime)s %(levelname)s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
-
     dns_check_result = dns_check(service=service)
     if dns_check_result["success"] == True:
         ip_address = dns_check_result["ip_address"]
         duration = dns_check_result["duration"]
-        logging.info(f"DNS check passed: {service} {ip_address} {duration:.2f}s")
+        logger.info(f"DNS check passed: {service} {ip_address} {duration:.2f}s")
     else:
         error = dns_check_result["error"]
-        logging.error(f"DNS check failed: {service} {error}")
+        logger.error(f"DNS check failed: {service} {error}")
 
     tcp_check_result = check_tcp_connection(host=service, port=443)
     if tcp_check_result["success"] == True:
         duration = tcp_check_result["duration"]
-        logging.info(f"TCP check passed: {service} {duration:.2f}s")
+        logger.info(f"TCP check passed: {service} {duration:.2f}s")
     else:
         message = tcp_check_result["message"]
-        logging.error(f"TCP check failed: {service} {message}")
+        logger.error(f"TCP check failed: {service} {message}")
 
     http_check_result = make_http_request(service=service)
     status_code = http_check_result["status_code"]
     duration = http_check_result["duration"]
     if status_code == 200:
-        logging.info(f"HTTP check passed: {service} {status_code} {duration:.2f}s")
+        logger.info(f"HTTP check passed: {service} {status_code} {duration:.2f}s")
     elif status_code < 500:
-        logging.warning(f"Unexpected HTTP result: {service} {status_code} {duration:.2f}s")
+        logger.warning(f"Unexpected HTTP result: {service} {status_code} {duration:.2f}s")
     else:
-        logging.error(f"HTTP check failed: {service} {status_code} {duration:.2f}s")
+        logger.error(f"HTTP check failed: {service} {status_code} {duration:.2f}s")
     
---
+++ src/service_health_checker/logging_config.py
@@ -0,0 +1,18 @@
+import logging
+
+logger = logging.getLogger("health_check_logger")
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+formatter.datefmt = '%Y-%m-%d %H:%M:%S'
+
+file_handler = logging.FileHandler('health_check.log', mode='a')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)

18d257b77fc6182f1a9f971f82522ddb633dff0d
Oluwasegun Ige <mailshegze@gmail.com>
2026-09-25 15:15:52 +0100

    refactor: http check to recieve full url

--- src/service_health_checker/health_check.py
+++ src/service_health_checker/health_check.py
@@ -6,11 +6,11 @@ from .dns_check import check_dns
 from .check_tcp_connection import check_tcp_connection
 from .make_http_request import make_http_request
 
-def health_check(
+def healthcheck(
     service: Annotated[str, typer.Argument(help="The services to be checked")] = "",
     port: Annotated[int, typer.Option(help="The port on which to attempt TCP connection")] = 443,
     timeout: Annotated[float, typer.Option(help="The timeout duration for the TCP check")] = 3.0,
-    endpoint: Annotated[str, typer.Option(help="The HTTP health check endpoint e.g /health")] = "/"
+    healthurl: Annotated[str, typer.Option(help="The complete HTTP health check url")] = ""
 ):
     results = {}
 
@@ -31,7 +31,7 @@ def health_check(
     else:
         logger.error(f"TCP check failed: {service} {tcp_check.duration:.2f}s {tcp_check.error}")
     
-    http_check = make_http_request(service=service, endpoint=endpoint)
+    http_check = make_http_request(service=service, healthurl=healthurl)
     results["http"] = http_check.success
     if http_check.success == True:
         logger.info(f"HTTP check passed: {service} 200 {http_check.duration:.2f}s")
